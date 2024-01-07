49164b62b2db3501f826be72bf41c9e7aef8dfc3
Achmad Fauzan <achmad4512@gmail.com>
2024-01-07 18:54:43 +0700

    commit

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ io.on('connection', socket => {
 
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}.`);
